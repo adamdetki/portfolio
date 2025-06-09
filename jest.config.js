9fa9c11c486021ada5b981cbb8d963ea31bc4737
Adam Detki <adam.detki@pm.me>
2025-06-09 12:18:48 +0200

    Added new hook and configured testing environment

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+};
