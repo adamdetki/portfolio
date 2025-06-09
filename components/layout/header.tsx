ad51d6a8fa25e95cc48e852f924cc676e72286b6
Adam Detki <adam.detki@pm.me>
2025-06-09 18:02:08 +0200

    Updated header

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -3,10 +3,13 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <Flex className="flex-row sticky top-0 z-50 bg-white shadow-sm">
-      <nav className="ml-auto p-4">
-        <Link href="/" className="mr-4">
-          Home
+    <Flex className="flex-row sticky top-0 z-50 bg-white shadow-sm justify-end">
+      <nav className="flex gap-4 p-4">
+        <Link href="/projects" className="font-bold">
+          Projects
+        </Link>
+        <Link href="/blog" className="font-bold">
+          Blog
         </Link>
       </nav>
     </Flex>
