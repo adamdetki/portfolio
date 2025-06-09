0220c35b70ddcc0770b1bfb97484245942d34083
Adam Detki <adam.detki@pm.me>
2025-06-09 17:11:17 +0200

    Updated Link

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,12 +1,13 @@
 import { Flex } from "@components/shared";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <Flex className="flex-row sticky top-0 z-50 bg-white shadow-sm">
       <nav className="ml-auto p-4">
-        <a href="/" className="mr-4">
+        <Link href="/" className="mr-4">
           Home
-        </a>
+        </Link>
       </nav>
     </Flex>
   );
